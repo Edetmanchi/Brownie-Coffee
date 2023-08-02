f71f07e1b0eb53dd0faaa057fd6d57ca8ae7fae2
Edet Effiong <greatstory247@gmail.com>
2023-08-02 17:20:32 +0100

    aded

---
+++ indexb.js
@@ -0,0 +1,56 @@
+// RESPONSIVE MENU FUNCTIONALITY
+// let menu = document.queryselector("#menu-btn"); 
+let navbar = document.queryselector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () =>{
+    menu.classList.toggle("#menu-bars")
+     navbar.classList.toggle("active");
+     About.classList{
+      
+     }
+}
+// another try
+var hamburger =document.getElementById("#menu-bars");
+hamburger.addEventListener("click", function(){
+    document.
+})
+
+
+
+
+
+// cart menu funtionality
+
+// let CartItem = ddocument.queryselector(".cart-items-container");
+
+// document.querySelector("#cart").onclick = () =>{
+//     CartItem
+//     .classList.toggle(".active")
+// }
+// another try
+var cart =document.getElementById("#cart");
+cart.onclick.addEventListener("click", function(){
+    document.getElementsByClassName("cart").classList.toggle(".cart-items-container")
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// for dark-mode functionality
+
+var icon = document.querySelector(".dark-mode-icon");
+icon.onclick = function viewMode(){
+    document.body.classList.toggle(".dark-mode")
+} 
+var icon = document.querySelector(".dark-mode-icon");
+function viewMode(){
+    if(ducument.getElementByClassName(".dark-mode-icon").onclick)
+}
